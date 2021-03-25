6f18ecf0d16845358e4826199a06081fe8571b61
David Brenner <70149349+davidbrenner1399@users.noreply.github.com>
2021-03-25 18:44:08 -0500

    Updated login page
    Can't really figure how to do this shit without deleting and just readding. Got fed up trying but here ya go.

---
+++ admin.php
@@ -0,0 +1,69 @@
+<?php
+    require_once("secrets.php");
+    session_start();
+
+
+
+    if(isset($_POST["Login"]))
+    {
+        if(empty($_POST["user_id"]) || empty($_POST["password"]))
+        {
+            $message = '<label>All fields required to enter</label>';
+        }
+        else
+        {
+            $id = $_POST['user_id'] ;
+            $pswd = $_POST['password'] ;
+
+            $sql = ("SELECT * FROM Associate WHERE Associate.User_Id = $id AND Associate.Password = '$pswd' AND Admin = 1 ;") ;
+            $login_query = $pdo->query($sql);
+
+            $rows = $login_query->fetch(PDO::FETCH_ASSOC);
+
+            $count = $login_query->rowCount();
+
+            if($count > 0)
+            {
+                $_SESSION["user_id"] = $_POST["user_id"];
+                header("Location: views.php");
+            }
+            else
+            {
+                $message = '<label>Username OR Password is incorrect</label>';
+            }
+        }
+    }
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset = "utf-8">
+    <title>ADMIN PORTAL</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    
+        <form class="box" method="post">
+            <h1>ADMIN PORTAL<h1>
+            <input id="text" type="text" name="user_id" placeholder="USER ID" ><br>
+            <input id="text" type="password" name="password" placeholder="PASSWORD"><br>
+
+            <input id="button" type="submit" name="Login" value="Login">  
+        
+        </form>
+
+    
+    
+    
+
+</body>
+
+</html>
