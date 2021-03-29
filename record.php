33651df818b2b745e2fe23c5e9b152a1e066a214
michelle-mich11 <61030034+michelle-mich11@users.noreply.github.com>
2021-03-29 05:31:53 -0500

    Add files via upload

---
+++ record.php
@@ -0,0 +1,30 @@
+<?php
+    require_once("session.php");
+    require_once("secrets.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+        <head>
+          <title>Login Form</title>
+          <link rel="stylesheet" href="associate.css">
+        </head>
+        <body>
+          <!--nav bar-->
+        <?php require "navbar-record.php"
+
+        ?>
+        <div>
+        <h1>Still working on Record.php :.(</h1>
+        <style type="text/css">
+          .hi {
+            padding: 0;
+            margin: 0;
+            font-family: montserrat;
+            color: blue;
+          }
+        </style>
+        </div>
+     </body>
+</html>
