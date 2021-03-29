0b6972ca36b04e0b05b7f2ec074babbf51bd5342
michelle-mich11 <61030034+michelle-mich11@users.noreply.github.com>
2021-03-29 05:21:55 -0500

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Navbar</title>
+
+  <style type="text/css">
+    *{
+      color: white;
+      padding: 0;
+      margin: 0;
+      font-family: sans-serif;
+    }
+    .navbar {
+      width: 100%;
+      background: #2b3c4e;
+    }
+
+    .navbar ul{
+      list-style-type: none;
+      text-align: right;
+    }
+
+    .navbar li {
+      display: inline-block;
+      font-size: 20px;
+      transition: .5s;
+      padding: 30px;
+    }
+
+  </style>
+</head>
+<body>
+  <div class="navbar">
+    <ul>
+      <li><a href="http://students.cs.niu.edu/~z1821488/admin.php">Admin Portal</a></li>
+      <li><a href="associate.php">Login</a></li>
+    </ul>
+  </div>
+</body>
+</html>
