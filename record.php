37a9a378beea05b11f9b81e7f87ad3c7e8d65522
michelle-mich11 <61030034+michelle-mich11@users.noreply.github.com>
2021-03-31 03:06:09 -0500

    Update record.php

--- record.php
+++ record.php
@@ -7,24 +7,33 @@
 <!DOCTYPE html>
 <html>
         <head>
-          <title>Login Form</title>
-          <link rel="stylesheet" href="associate.css">
+          <title>Quote Record Form</title>
+          <link rel="stylesheet" href="record.css">
         </head>
         <body>
           <!--nav bar-->
         <?php require "navbar-record.php"
 
         ?>
-        <div>
-        <h1>Still working on Record.php :.(</h1>
-        <style type="text/css">
-          .hi {
-            padding: 0;
-            margin: 0;
-            font-family: montserrat;
-            color: blue;
-          }
-        </style>
-        </div>
-     </body>
+        <div id="content">
+            <div class="tile">
+              <h3>Customers</h3>
+              <a href="customer.php">
+                <section>
+                  <div class=banner style="background-image: url('https://ciklopea.com/wp-content/uploads/2018/05/Customer-Experience-800x600.png');"></div>
+                </section>
+              </a>
+              <a href="customer.php"><p>View Current Customers</p></a>
+            </div>
+            <div class="tile">
+              <h3>Quote Form</h3>
+              <a href="quote-form.php">
+                <section>
+                  <div class=banner style="background-image: url('https://y26uq11r8xr1zyp0d3inciqv-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/53-1.jpg');"></div>
+                </section>
+              </a>
+              <a href="quote-form.php"><p>View Quote Form</p></a>
+            </div>
+          </div>
+        </body>
 </html>
