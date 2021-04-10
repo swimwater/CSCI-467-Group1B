5f17080edb3f7626a71ddecc685e71bed7384786
davidbrenner1399 <davidbrenner1399@gmail.com>
2021-04-09 21:04:54 -0500

    created page to process orders.

---
+++ processOrders.php
@@ -0,0 +1,23 @@
+<?php
+
+  require("session.php");
+ 
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>PROCESS ORDERS</title>
+<link rel="stylesheet" href="associateManageQuote.css">
+</head>
+
+<body>
+    <h1>Process Orders</h1>
+
+
+
+
+</body>
+</html>
