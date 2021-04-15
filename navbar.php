444db00e1892a891199cfac61c735607b8e5f4d8
michelle-mich11 <61030034+michelle-mich11@users.noreply.github.com>
2021-04-15 16:12:03 -0500

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,41 +1,114 @@
+<?php
+    require_once("session.php");
+    require_once("secrets.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Navbar</title>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <style type="text/css">
-    *{
-      color: white;
-      padding: 0;
-      margin: 0;
-      font-family: sans-serif;
-    }
-    .navbar {
-      width: 100%;
-      background: #2b3c4e;
-    }
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="customer.css">
 
-    .navbar ul{
-      list-style-type: none;
-      text-align: right;
-    }
+    <title>Dropdown Navigation Bar</title>
 
-    .navbar li {
-      display: inline-block;
-      font-size: 20px;
-      transition: .5s;
-      padding: 30px;
-    }
+  </head>
 
-  </style>
-</head>
-<body>
-  <div class="navbar">
-    <ul>
-      <li><a href="http://students.cs.niu.edu/~z1821488/admin.php">Admin Portal</a></li>
-      <li><a href="associate.php">Login</a></li>
-    </ul>
-  </div>
-</body>
+  <body>
+    <?php if($_SESSION['pos'] == 0):?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Welcome</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggle-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="record.php">Home</a>
+                </li>
+                <li class="nav">
+                    <a class="nav-link" href="customer.php">Customer</a>
+                </li>
+                <li class="nav">
+                    <a class="nav-link" href="quote-form.php">Quote Form</a>
+                </li>
+                <li class="nav">
+                    <a class="nav-link" href="manageUnfinalQuotesHeader.php">View Unfinalized Quotes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <?php endif;?>
+    <?php if($_SESSION['pos'] == 1):?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Welcome</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggle-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="nav-link" href="manageFinalQuotesHeader.php">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav">
+                    <a class="nav-link" href="manageFinalQuotesHeader.php">View Finalized Quotes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="viewSanctionQuotes.php">View Sanctioned Quotes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <?php endif;?>
+    <?php if($_SESSION['pos'] == 2):?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Welcome</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item ">
+                    <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="views.php">View Quotes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="manage.php">Manage Associate Records</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="transferQuote.php">Transfer Quotes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="record.php" id="navbarDropdown">Associate Portal</a>
+                </li>
+                <li class="nav-item nav-item">
+                    <a class="nav-link" href="viewSanctionQuotes.php" id="navbarDropdown">HQ Portal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+        </nav>
+    <?php endif;?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
