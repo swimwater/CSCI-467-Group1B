7d1840336209953bdae7c55740e8e822a4c2a857
michelle-mich11 <61030034+michelle-mich11@users.noreply.github.com>
2021-04-15 17:11:49 -0500

    Update multiUserLogin.php

--- multiUserLogin.php
+++ multiUserLogin.php
@@ -2,7 +2,6 @@
     require_once("secrets.php");
     session_start();
 
-
     if(isset($_POST["Login"])) {
         if(empty($_POST["user_id"]) || empty($_POST["password"])) {
             $msg = '<div style="text-align: center">ALL FIELDS REQUIRED.</div>';
@@ -13,7 +12,7 @@
                 $id = $_POST['user_id'];
                 $pswd = $_POST['password'];
 
-                $getLogin = "select User_Id, Password, Pos from Associate where User_Id = ".$id." and Password = '".$pswd."';";
+                $getLogin = "select User_Id, Password, Pos from Associate where User_Id = '".$id."' and Password = '".$pswd."';";
 
                 $login_query = $pdo->query($getLogin);
 
@@ -21,14 +20,17 @@
 
                 if($rows['User_Id'] == $id && $rows['Password'] == $pswd && $rows['Pos'] == 0) {
                     $_SESSION["user_id"] = $_POST["user_id"];
+                    $_SESSION['pos'] = 0;
                     header("Location: record.php");
                 }
                 else if ($rows['User_Id'] == $id && $rows['Password'] == $pswd && $rows['Pos'] == 1) {
                     $_SESSION["user_id"] = $_POST["user_id"];
+                    $_SESSION['pos'] = 1;
                     header("Location: manageFinalQuotesHeader.php");
                 }
                 else if ($rows['User_Id'] == $id && $rows['Password'] == $pswd && $rows['Pos'] == 2) {
                     $_SESSION["user_id"] = $_POST["user_id"];
+                    $_SESSION['pos'] = 2;
                     header("Location: views.php");
                 }
                 else {
@@ -50,14 +52,12 @@
         <meta charset="utf-8">
         <title>Multi User Login Form</title>
         <!-- CSS only -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="multiUserLogin.css">
+        <link rel="stylesheet" href="multiUserLogin.css">
     </head>
     <body>
         <!--page body-->
-        <div class="center">
+            <form class="center" method="post">
             <h1>Login Portal</h1>
-            <form method="post">
                 <div class="txt_field">
                     <input type="text" name="user_id" placeholder="User ID">
                 </div>
@@ -69,6 +69,5 @@
                     <a href="reset.php">Forgot Password?</a>
                 </div>
             </form>
-        </div>
     </body>
 </html>
