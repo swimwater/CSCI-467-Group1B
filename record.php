c1e76fe18715ba2cde1cc52cd94b1402f053454e
Ben Ford <Z1860966@students.niu.edu>
2021-04-16 17:46:54 -0500

    Added Bootstrap

--- record.php
+++ record.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
         <head>
+          <!--include bootstrap CSS via CDN and custom stylesheet --->
+      		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+      		<!--include jquery and bootstrap javascript via CDN --->
+      		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+      		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
           <title>Quote Record Form</title>
 
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
